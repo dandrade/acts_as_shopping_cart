3ca57c4e3dfa0e54bbc16de5e02f161ccf879d29
Daniel Andrade <dam@unixmexico.org>
2016-09-27 15:56:20 -0500

    Adds price sum for duplicate products

--- lib/active_record/acts/shopping_cart/collection.rb
+++ lib/active_record/acts/shopping_cart/collection.rb
@@ -11,6 +11,7 @@ module ActiveRecord
           if cart_item
             cumulative = cumulative == true ? cart_item.quantity : 0
             cart_item.quantity = (cumulative + quantity)
+            cart_item.price += price
             cart_item.save
             cart_item
           else
